0fe32f944201e8bbb7db8b44357b57e50c06f777
palbase-ci <ci@pallasite.net>
2026-06-11 12:35:18 +0000

    release: Palbe v0.9.2 (binary + docs)

--- Package.swift
+++ Package.swift
@@ -26,8 +26,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Palbe",
-            url: "https://github.com/palgroup/palbackend-ios/releases/download/v0.9.1/Palbe.xcframework.zip",
-            checksum: "d72c5bfb8191fbeff30438ebf4c33780af9e4dc3b955a529dd63a6b7d0c8fc74"
+            url: "https://github.com/palgroup/palbackend-ios/releases/download/v0.9.2/Palbe.xcframework.zip",
+            checksum: "040612f4f51a6a1a70f2178236aaba3510a1eaa2e6922d240ea98c621876d407"
         ),
     ],
     swiftLanguageModes: [.v6]
