38b0c538147d9df6b82400603a420a51c29d47d1
palbase-ci <ci@pallasite.net>
2026-07-19 19:08:13 +0000

    release: Palbe v0.27.0 (binary + codegen plugin source + docs)

--- Package.swift
+++ Package.swift
@@ -27,8 +27,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Palbe",
-            url: "https://github.com/palgroup/palbackend-ios/releases/download/v0.26.2/Palbe.xcframework.zip",
-            checksum: "71d5baa7f4468a9e29c242e998ecb6c5e91b3a6dcdbbd39e1e36915e46496879"
+            url: "https://github.com/palgroup/palbackend-ios/releases/download/v0.27.0/Palbe.xcframework.zip",
+            checksum: "b9b188a59f6c85c454b4892f9033fe439f7375f357e67609d86d9d104fcdfae3"
         ),
         .binaryTarget(
             name: "RustLiveKitUniFFI",
@@ -37,8 +37,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "LiveKitWebRTC",
-            url: "https://github.com/livekit/webrtc-xcframework/releases/download/144.7559.10/LiveKitWebRTC.xcframework.zip",
-            checksum: "a0f0d5e80b2a25fa6d9e8d235b90e426ec44ca872e3331c6c3d03d2fccc8a2e6"
+            url: "https://github.com/livekit/webrtc-xcframework/releases/download/144.7559.11/LiveKitWebRTC.xcframework.zip",
+            checksum: "07c5caf718058af3c528dcabd257298c40e5a8527e4fb9f47c48336ba5899853"
         ),
         .executableTarget(name: "palbase-swiftgen", path: "Sources/palbase-swiftgen"),
         .plugin(
