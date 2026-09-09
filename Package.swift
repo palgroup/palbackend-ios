e70589df8869c09b364648b584410dfffc3975ee
palbase-ci <ci@pallasite.net>
2026-09-09 11:16:26 +0000

    release: Palbe v0.58.6 (binaries + generator source + docs)

--- Package.swift
+++ Package.swift
@@ -27,23 +27,23 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Palbe",
-            url: "https://github.com/palgroup/palbackend-ios/releases/download/v0.58.5/Palbe.xcframework.zip",
-            checksum: "97cba268a5c906e3a2e2c7011791ce646c564cf4ba8e3374e0e84b3e48db9072"
+            url: "https://github.com/palgroup/palbackend-ios/releases/download/v0.58.6/Palbe.xcframework.zip",
+            checksum: "3c7a054cc89d09ddace2b987b7c7e0d02c173b9fbe2d732462e956e09855abbe"
         ),
         .binaryTarget(
             name: "PalbeMessaging",
-            url: "https://github.com/palgroup/palbackend-ios/releases/download/v0.58.5/PalbeMessaging.xcframework.zip",
-            checksum: "d08b518adb319b42993280aa455a0986c694bcc75a7a9414eab3af68ecfc7661"
+            url: "https://github.com/palgroup/palbackend-ios/releases/download/v0.58.6/PalbeMessaging.xcframework.zip",
+            checksum: "15fdd02d020d5c00d1bfae5410a7b454025971be2ab4fce0eb253d8c375ac33f"
         ),
         .binaryTarget(
             name: "PalbeCall",
-            url: "https://github.com/palgroup/palbackend-ios/releases/download/v0.58.5/PalbeCall.xcframework.zip",
-            checksum: "c713398137e471c72db9445797dee37bca70f35b7bf1123925f3acc50d548a7a"
+            url: "https://github.com/palgroup/palbackend-ios/releases/download/v0.58.6/PalbeCall.xcframework.zip",
+            checksum: "81b8b939df046ff5899c8c6a882f940c9179e6400799d50d358db809c7a4dc06"
         ),
         .binaryTarget(
             name: "PalbePurchases",
-            url: "https://github.com/palgroup/palbackend-ios/releases/download/v0.58.5/PalbePurchases.xcframework.zip",
-            checksum: "08c877b423afe9e22bdb913412941d7896293c9508426e2ca6042dc8b1b145c1"
+            url: "https://github.com/palgroup/palbackend-ios/releases/download/v0.58.6/PalbePurchases.xcframework.zip",
+            checksum: "836312eb76d2eea23d801bdc20c45b3cf098e938d415a98abe00453c94463a0a"
         ),
         .binaryTarget(
             name: "RustLiveKitUniFFI",
